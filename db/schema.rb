680c6f81bfe2a850a2f7d1f9a76ff782c73e3d17
Sujit K M <kmsujit@gmail.com>
2026-04-11 10:50:08 +0530

    Revert "Fix for Dependabot Alerts"
    This reverts commit a978598cfa6452203c8c60bad5c2b754f4b7d161.

--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2024_09_25_025344) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_25_025344) do
   create_table "user_associations", force: :cascade do |t|
+    t.integer "user_to"
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
-    t.integer "user_to"
     t.index ["user_id"], name: "index_user_associations_on_user_id"
   end
 
   create_table "user_sleeps", force: :cascade do |t|
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_user_sleeps_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "user_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "user_name"
   end
 
   add_foreign_key "user_associations", "users"
